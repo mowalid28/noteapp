7b34ff8de4cfca7dfffc3f388370bbcce5ada3d5
Mohamed28102002 <mohamedwalid28102002@gmail.com>
2024-09-30 14:41:01 +0300

    generate model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,21 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+//normal to be error becasue still not created yet
+//will generate this file adapter for this model must write name correctly
+@HiveType(typeId: 0)
+//for your model and all about 255
+//here place to generate
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  //unique per class
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
