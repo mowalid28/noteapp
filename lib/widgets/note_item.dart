c82c8e56c82e57629c78b9c6b911b5906b4c9343
Mohamed28102002 <mohamedwalid28102002@gmail.com>
2024-09-30 10:45:37 +0300

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/views/edit_note_view.dart';
 
 class noteItem extends StatelessWidget {
   const noteItem({super.key});
@@ -8,54 +9,66 @@ class noteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10.0),
-      child: Container(
-        padding: EdgeInsets.only(top: 16, bottom: 16, left: 12),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.amber,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                'flutter note',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 16),
-                child: Text(
-                  'this is the subtitel of noteayathis is the subtitel of noteaya',
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const EditNoteView();
+              },
+            ),
+          );
+        },
+        child: Container(
+          padding: EdgeInsets.only(top: 16, bottom: 16, left: 12),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  'flutter note',
                   style: TextStyle(
-                    color: Colors.black.withOpacity(0.5),
-                    fontSize: 17,
+                    color: Colors.black,
+                    fontSize: 26,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                  child: Text(
+                    'this is the subtitel of noteayathis is the subtitel of noteaya',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.5),
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
                   ),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
+                child: Text(
+                  'May 21,2024',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24.0),
-              child: Text(
-                'May 21,2024',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
